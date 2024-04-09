61ec20edb4d6d089c32890f0820eaf656ae72a3f
伯大成 <bodacheng123@gmail.com>
2024-04-09 11:23:00 +0900

    相机抵挡改为球射线检测

--- Assets/Camera/VisibilityControl.cs
+++ Assets/Camera/VisibilityControl.cs
@@ -1,20 +1,29 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class VisibilityControl : MonoBehaviour
 {
-    [SerializeField] float radius = 5.0f; // 可见性控制半径
+    [SerializeField] float radius = 5f; // SphereCast的半径
+    [SerializeField] float detectDis = 50f; // 可见性控制半径
     [SerializeField] LayerMask layerMask; // 可以在Inspector中指定的层掩码
+    [SerializeField] LayerMask unitLayer;
+    [SerializeField] int wallLayer;
     [SerializeField] int detectInterval = 10;
+    [SerializeField] int rayCastMax = 20;
     private List<Renderer> _hiddenObjects = new List<Renderer>(); // 存储当前被隐藏的物体
     private int _frameCounter = 0;
     
     // 预先分配一个足够大的数组，以存储可能检测到的所有collider
     // 数组的大小取决于你预计场景中可能同时存在的最大collider数量
-    readonly Collider[] _hitColliders = new Collider[600]; 
-    
+    private RaycastHit[] _hitColliders;
+
+    void Start()
+    {
+        _hitColliders = new RaycastHit[rayCastMax];
+    }
+
     public void LocalUpdate()
     {
         if (_frameCounter < detectInterval)
@@ -24,17 +33,32 @@ public class VisibilityControl : MonoBehaviour
         }
         _frameCounter = 0;
         
+        Ray ray = new Ray(transform.position, transform.forward);
+        
         // 获取实际检测到的collider数量
-        int numColliders = Physics.OverlapSphereNonAlloc(transform.position, radius, _hitColliders, layerMask);
+        int numColliders = Physics.SphereCastNonAlloc(ray, radius, _hitColliders, detectDis, layerMask);
         List<Renderer> thisFrameDetected = new List<Renderer>();
-        for (int i = 0; i < numColliders; i++)
+        
+        if (numColliders > 0)
         {
-            GameObject obj = _hitColliders[i].gameObject;
-            Renderer renderer = obj.GetComponent<Renderer>();
-            if (renderer != null)
+            var hitList = _hitColliders.ToList().FindAll(x=> x.collider);
+            var unitTargets = hitList.FindAll(x=> ((unitLayer & (1 << x.collider.gameObject.layer)) != 0));
+            var wallTargets = hitList.FindAll(x=> x.collider.gameObject.layer == wallLayer);
+            if (unitTargets.Count > 0 && wallTargets.Count > 0)
             {
-                renderer.enabled = false; // 禁用Renderer组件
-                thisFrameDetected.Add(renderer); // 将对象添加到隐藏对象集合中
+                var minDis = unitTargets.Min(x=> Vector3.Distance(x.transform.position, transform.position));
+                foreach (var wall in wallTargets)
+                {
+                    if (Vector3.Distance(wall.transform.position, transform.position) < minDis)
+                    {
+                        Renderer renderer = wall.transform.GetComponent<Renderer>();
+                        if (renderer != null)
+                        {
+                            renderer.enabled = false; // 禁用Renderer组件
+                            thisFrameDetected.Add(renderer); // 将对象添加到隐藏对象集合中
+                        }
+                    }
+                }
             }
         }
         
@@ -51,12 +75,5 @@ public class VisibilityControl : MonoBehaviour
     public void Clear()
     {
         _hiddenObjects.Clear();
-    }
-    
-    // 当对象被选中时在Scene视图中绘制一个表示半径的球体
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow; // 设置绘制颜色为黄色
-        Gizmos.DrawWireSphere(transform.position, radius); // 绘制一个中心在物体位置、半径为radius的线框球体
     }
 }
